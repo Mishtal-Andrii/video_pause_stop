9a8c5c8122606d5ca50a5b949a2155c1480c88a1
Andrii Mishtal <mishtal.andriy@gmail.com>
2021-01-22 14:20:01 +0200

    finish

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # video_pause_stop
+[DEMO LINK]( https://mishtal-andrii.github.io/video_pause_stop_preloader/.)
--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const btn = document.querySelector(".switch-btn");
 const video = document.querySelector(".video-container");
 
-btn.addEventListener("click", function () {
-  if (!btn.classList.contains("slide")) {
-    btn.classList.add("slide");
+btn.addEventListener('click', () => {
+  if (!btn.classList.contains('slide')) {
+    btn.classList.add('slide');
     video.pause();
-  } else {
-    btn.classList.remove("slide");
+  } 
+  else {
+    btn.classList.remove('slide');
     video.play();
   }
-});
+})
 
-// preloader
-const preloader = document.querySelector(".preloader");
+//Preloader
+const preloader = document.querySelector('.preloader')
 
-window.addEventListener("load", function () {
-  preloader.classList.add("hide-preloader");
-});
+window.addEventListener('load', () => {
+  preloader.classList.add('hide-preloader')
+})
